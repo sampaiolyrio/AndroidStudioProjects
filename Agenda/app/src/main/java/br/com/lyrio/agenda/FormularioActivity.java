f0a541306c5258647b910ffd1fc7afb9bb2a96a8
Marcos Lyrio <sampaiolyrio@hotmail.com>
2017-11-29 00:50:14 -0200

    Curso: Crie sua App fantástica com Android Studio Aula: 3.3 Signed-off-by: Marcos Lyrio <sampaiolyrio@hotmail.com>

--- Agenda/app/src/main/java/br/com/lyrio/agenda/FormularioActivity.java
+++ Agenda/app/src/main/java/br/com/lyrio/agenda/FormularioActivity.java
@@ -1,10 +1,14 @@
 package br.com.lyrio.agenda;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -15,14 +19,35 @@ public class FormularioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
-            Button botaoSalvar  = (Button) findViewById(R.id.formulario_salvar);
+        Button botaoSalvar  = (Button) findViewById(R.id.formulario_salvar);
         botaoSalvar.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 Toast.makeText(FormularioActivity.this,"Botão Clicado",Toast.LENGTH_SHORT).show();
+                finish();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_formulario, menu);
+        return super.onCreateOptionsMenu(menu);
 
     }
 
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.menu_formulario_ok:
+                    Toast.makeText(FormularioActivity.this, "Aluno Salvo", Toast.LENGTH_SHORT).show();
+                    finish();
+                    break;
+            }
+            return super.onOptionsItemSelected(item);
+
+    }
 }
+
